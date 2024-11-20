94ab3452391244241ec9752158594eec076f3ad5
Raymond <akdfhr2@gmail.com>
2024-11-20 17:24:44 +0900

    fix: 리뷰 내용 적용하기
    - as 타입 narrowing하기
    - children props ReactNode로 변경하기

--- packages/ui/typography/src/Typography.tsx
+++ packages/ui/typography/src/Typography.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 type TextTags = "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "strong" | "em" | "small" | "q" | "u";
 
-type TypographyProps<T extends TextTags = TextTags> = {
-  as?: T;
-  children: string;
-} & Omit<React.HTMLAttributes<HTMLElement>, "color" | "as" | "variant">;
+type ElementType<P = any, Tag extends TextTags = TextTags> =
+  | { [K in Tag]: P extends JSX.IntrinsicElements[K] ? K : never }[Tag]
+  | React.ComponentType<P>;
 
-const Typography = React.forwardRef(<T extends TextTags>(props: TypographyProps<T>, ref: React.ForwardedRef<HTMLElement>) => {
-  const { as: Comp = "span" as any, children, ...rest } = props;
+type TypographyProps<E extends ElementType> = { as?: E };
+
+type Props<E extends ElementType> = TypographyProps<E> & Omit<React.HTMLAttributes<HTMLElement>, "color" | "as" | "variant">;
+
+const Typography = React.forwardRef(<T extends ElementType>(props: Props<T>, ref: React.ForwardedRef<HTMLElement>) => {
+  const { as: Comp = "span", children, ...rest } = props;
 
   return (
     <Comp {...rest} ref={ref}>
